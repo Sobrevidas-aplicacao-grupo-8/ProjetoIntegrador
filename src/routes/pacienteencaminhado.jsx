01c149f1a68413d6aa912dcd59d24532710602fa
claudio <claudio23@discente.ufg.br>
2024-05-24 17:44:11 -0300

    pacienteencaminhado

---
+++ src/routes/pacienteencaminhado.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Style/pacienteencaminhado.css";
+import { useNavigate } from "react-router-dom";
+const Pacienteencaminhado = () => {
+  return (
+    <div className="v196_125">
+    <div className="v196_126"></div>
+    <span className="v196_127">SOBREVIDAS ACS</span>
+    <span className="v196_132">voltar</span>
+    <div className="v196_165"></div>
+    <span><input className="v196_166" type="search" placeholder="Digite o nome do paciente..." /></span>
+    <span className="v196_167">Pacientes encaminhados</span>
+    <div className="v202_6"></div>
+    <div className="v202_7"></div>
+    <div className="v202_8"></div>
+    <div className="v202_9"></div>
+    <div className="v202_10"></div>
+    <div className="v202_11"></div>
+    <div className="v202_12"></div>
+    <div className="v202_13"></div>
+    <div className="v202_14"></div>
+    <span className="v202_15">Pedro</span>
+    <span className="v202_16">Jorge</span>
+    <span className="v202_17">João</span>
+    <span className="v202_18">Alexandre</span>
+    <span className="v202_19">José</span>
+    <span className="v202_20">Marcos</span>
+    <span className="v202_21">Arthur</span>
+    <span className="v202_22">Vinicius</span>
+    <span className="v202_23">Guilherme</span>
+</div>
+  );
+};
+
+export default Pacienteencaminhado;
